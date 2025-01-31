0946ad80d0f93c6704307b77db1385f7ab3970c1
nada7harby <87335429+nada7harby@users.noreply.github.com>
2025-01-31 22:35:43 +0200

    posts

--- js/main.js
+++ js/main.js
@@ -1,31 +1,24 @@
-
 // 887e6bbe201889ebca380d3b230f07cd
-const apiKey = "887e6bbe201889ebca380d3b230f07cd"; 
-
-
+const apiKey = "887e6bbe201889ebca380d3b230f07cd";
 
 // ******************************* get today date (in right side) **************************************
-const today = new Date().toLocaleDateString(); 
-const todayPara = document.getElementById('today');
+const today = new Date().toLocaleDateString();
+const todayPara = document.getElementById("today");
 
-console.log(today); 
+console.log(today);
 todayPara.innerHTML = today;
-
-
 
 // // ******************************* handle select city **************************************
 
-// const citiesUrl = "https://countriesnow.space/api/v0.1/countries"; 
-
+// const citiesUrl = "https://countriesnow.space/api/v0.1/countries";
 
 // const selectCity = document.getElementById("city");
-
 
 // // fetch cities and display it
 // fetch(citiesUrl).then(res => res.json()).then(data => {
 //   // console.log(data.data);
 //   data.data.forEach(element => {
-    
+
 //     if(element.country === 'Egypt'){
 //       // console.log(element);
 //       console.log(element.cities);
@@ -42,14 +35,11 @@ todayPara.innerHTML = today;
 
 // }).catch(err => console.log("there is error : ",err));
 
-
-
-
 // // city that user will select
 // selectCity.addEventListener("change", (event) => {
 
 //   const selectedCity = event.target.value;       // new city the user selected
-  
+
 //   if (selectedCity !== "Select City") {
 //     const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&appid=${apiKey}&units=metric`;
 
@@ -68,99 +58,83 @@ todayPara.innerHTML = today;
 //   }
 // });
 
-
-
-
-const citiesUrl = "https://countriesnow.space/api/v0.1/countries"; 
+const citiesUrl = "https://countriesnow.space/api/v0.1/countries";
 const selectCity = document.getElementById("city");
-const countryLinks = document.querySelectorAll('.item a'); 
+const countryLinks = document.querySelectorAll(".item a");
 
 let countriesData = [];
 
-
 fetch(citiesUrl)
-  .then(res => res.json())
-  .then(data => {
-    countriesData = data.data; 
+  .then((res) => res.json())
+  .then((data) => {
+    countriesData = data.data;
   })
-  .catch(err => console.log("هناك خطأ: ", err));
-
-
+  .catch((err) => console.log("هناك خطأ: ", err));
 
 function updateCities(countryName) {
-  selectCity.innerHTML = `<option>Select City</option>`; 
+  selectCity.innerHTML = `<option>Select City</option>`;
 
-  const country = countriesData.find(c => c.country.toLowerCase() === countryName.toLowerCase());
+  const country = countriesData.find(
+    (c) => c.country.toLowerCase() === countryName.toLowerCase()
+  );
 
   console.log("country =====>");
   console.log(country);
 
-
   if (country) {
-    country.cities.forEach(city => {
-      const option = document.createElement('option');
+    country.cities.forEach((city) => {
+      const option = document.createElement("option");
       option.textContent = city;
       selectCity.appendChild(option);
     });
   }
 }
 
-
-
-countryLinks.forEach(link => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault(); 
-    const countryName = link.getAttribute('data-country'); 
+countryLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const countryName = link.getAttribute("data-country");
     updateCities(countryName);
 
     document.getElementById("forecast").scrollIntoView({ behavior: "smooth" });
   });
 });
 
-
-
-
 selectCity.addEventListener("change", (event) => {
-  const selectedCity = event.target.value;    
-  
+  const selectedCity = event.target.value;
+
   if (selectedCity !== "Select City") {
     const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&appid=${apiKey}&units=metric`;
 
     fetch(weatherUrl)
-      .then(response => {
+      .then((response) => {
         if (!response.ok) {
           throw new Error("City weather data not found!");
         }
         return response.json();
       })
-      .then(data => {
+      .then((data) => {
         updateWeatherUI(data);
       })
-      .catch(error => console.error("Error fetching weather:", error));
+      .catch((error) => console.error("Error fetching weather:", error));
   }
 });
 
-
-
-
 // display data in ui
 function updateWeatherUI(data) {
-
-
   // ************** temp & cond for selected city (in left side) ****************
-  const temperatureElement = document.getElementById('currentTemp');
-  const conditionElement = document.getElementById('currentCond');
+  const temperatureElement = document.getElementById("currentTemp");
+  const conditionElement = document.getElementById("currentCond");
 
   temperatureElement.textContent = `${data.main.temp}°C`;
   conditionElement.textContent = data.weather[0].description;
 
-
   // ************** 5 data for selected city (in left side) ****************
-  const humidityElement = document.getElementById('humidity');
-  const windElement = document.getElementById('wind');
-  const rainElement = document.getElementById('rain');
-  const sunriseElement = document.getElementById('sunrise');
-  const sunsetElement = document.getElementById('sunset');
+  const humidityElement = document.getElementById("humidity");
+  const windElement = document.getElementById("wind");
+  const rainElement = document.getElementById("rain");
+  const sunriseElement = document.getElementById("sunrise");
+  const sunsetElement = document.getElementById("sunset");
 
   humidityElement.textContent = `Humidity: ${data.main.humidity}%`;
   windElement.textContent = `Wind Speed: ${data.wind.speed} m/s`;
@@ -172,32 +146,27 @@ function updateWeatherUI(data) {
   const sunsetTime = new Date(data.sys.sunset * 1000).toLocaleTimeString();
   sunriseElement.textContent = `Sunrise: ${sunriseTime}`;
   sunsetElement.textContent = `Sunset: ${sunsetTime}`;
-
 }
-
-
 
 // *************************************** weekly weather *****************************************
 
-const city = "Assiut"; 
+const city = "Assiut";
 const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
 
-fetch(forecastUrl).then(response => response.json()).then(data => {
-
+fetch(forecastUrl)
+  .then((response) => response.json())
+  .then((data) => {
     const dailyForecasts = extractDailyForecasts(data.list);
 
     updateWeeklyWeather(dailyForecasts);
-
   })
-  .catch(error => console.error("Error fetching forecast data:", error));
-
+  .catch((error) => console.error("Error fetching forecast data:", error));
 
 // extract 7 days
 function extractDailyForecasts(forecastList) {
   const dailyData = {};
-  
-  forecastList.forEach(entry => {
 
+  forecastList.forEach((entry) => {
     const date = entry.dt_txt.split(" ")[0]; // extract only days
     if (!dailyData[date]) {
       dailyData[date] = {
@@ -207,14 +176,19 @@ function extractDailyForecasts(forecastList) {
         day: new Date(date).toLocaleDateString("en-US", { weekday: "short" }),
       };
     } else {
-      dailyData[date].minTemp = Math.min(dailyData[date].minTemp, entry.main.temp_min);
-      dailyData[date].maxTemp = Math.max(dailyData[date].maxTemp, entry.main.temp_max);
+      dailyData[date].minTemp = Math.min(
+        dailyData[date].minTemp,
+        entry.main.temp_min
+      );
+      dailyData[date].maxTemp = Math.max(
+        dailyData[date].maxTemp,
+        entry.main.temp_max
+      );
     }
   });
 
   return Object.values(dailyData).slice(0, 7); // return first 7 days
 }
-
 
 // update data in ui
 function updateWeeklyWeather(forecastData) {
@@ -223,33 +197,86 @@ function updateWeeklyWeather(forecastData) {
   forecastData.forEach((day, index) => {
     if (items[index]) {
       const img = items[index].querySelector("img");
-      const tempText = items[index].querySelectorAll("h4")[0];  
-      const dayText = items[index].querySelectorAll("h4")[1]; 
+      const tempText = items[index].querySelectorAll("h4")[0];
+      const dayText = items[index].querySelectorAll("h4")[1];
 
-      tempText.textContent = `${Math.round(day.maxTemp)}°C - ${Math.round(day.minTemp)}°C`;
+      tempText.textContent = `${Math.round(day.maxTemp)}°C - ${Math.round(
+        day.minTemp
+      )}°C`;
       dayText.textContent = day.day;
     }
   });
 }
 
-
-
-
-
 // *************************************** go to top btn *****************************************
 let goTopBtn = document.getElementById("go-to-top");
 
-
-window.onscroll = function() {
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-        goTopBtn.style.display = "flex"; 
-    } else {
-        goTopBtn.style.display = "none"; 
-    }
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    goTopBtn.style.display = "flex";
+  } else {
+    goTopBtn.style.display = "none";
+  }
 };
 
-goTopBtn.addEventListener("click", function() {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+goTopBtn.addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
+const apiKey2 = "b12959edcb9b40828f7ea8ae43ce86bb";
+const query = "weather";
+const url = `https://newsapi.org/v2/everything?q=${query}&apiKey=${apiKey2}&language=en&pageSize=10`;
+var articles;
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
 
+    articles = data.articles.slice(2, 5);
+    console.log(data.articles);
+    const container = document.getElementById("news-container");
+    articles.forEach((article) => {
+      const articleHTML = document.createElement("div");
+      articleHTML.classList.add("col-lg-4");
+      articleHTML.innerHTML = `
+
+<div class="card">
+          <img class="card-img-top" src="${article.urlToImage}" />
+          <div class="card-body">
+            <h5 class="card-title">${article.title}</h5>
+            <p class="card-text">By Sarah Lee • October 10, 2024</p>
+            <p class="card-text">
+             ${article.description}
+            </p>
+          <a style="cursor : pointer;" data-title="${article.title}" data-content="${article.content}" data-image="${article.urlToImage}"  class="btn move-head fw-bold read-more">Read More <i class="fa-solid fa-arrow-right"></i></a>
+            </a>
+          </div>
+</div>
+      `;
+      document.getElementById("blog-posts").appendChild(articleHTML);
+    });
+    const readMoreLinks = document.querySelectorAll(".read-more");
+    readMoreLinks.forEach((link) => {
+      link.addEventListener("click", function (e) {
+        e.preventDefault();
+        localStorage.setItem("blogTitle", link.getAttribute("data-title"));
+        localStorage.setItem("blogContent", link.getAttribute("data-content"));
+        localStorage.setItem("blogImage", link.getAttribute("data-image"));
+        window.location.href = "blog.html";
+      });
+    });
+    
+  })
+  .catch((error) => console.error("Error:", error));
+window.onload = function () {
+  document.body.style.backgroundColor = "white";
+  document.body.style.color = "black";
+
+  var isDark = localStorage.getItem("darkMode");
+  if (isDark === "true") {
+    enableDarkMode();
+  }
+};
