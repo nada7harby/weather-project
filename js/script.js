4e0cb4cb507468b4dfdd25ee15fead45e212a051
Haidy Atef Zakaria <atefhaidy60@gmail.com>
2025-01-31 17:20:29 +0200

    ز

--- js/script.js
+++ js/script.js
@@ -188,3 +188,21 @@ readMoreLinks.forEach(link => {
     
   })
   .catch(error => console.error('Error:', error));
+  window.onload = function () {
+    document.body.classList.remove('dark-mode');
+    var isDark = localStorage.getItem('darkMode');
+    if (isDark === 'true') {
+      document.body.classList.add('dark-mode');
+    }
+  };
+
+  function toggledarkmode() {
+    var body = document.body;
+    body.classList.toggle('dark-mode');
+
+    if (body.classList.contains('dark-mode')) {
+      localStorage.setItem('darkMode', 'true');
+    } else {
+      localStorage.removeItem('darkMode');
+    }
+  }
